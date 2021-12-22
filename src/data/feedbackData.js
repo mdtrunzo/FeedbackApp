f5f1dab5a7d8e9987b19833a3bf3658461ac4a69
Matias Daniel Trunzo <matiasdanieltrunzo@MacBook-Pro-de-Matias.local>
2021-12-22 15:40:21 -0300

    first commit

---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Elit excepteur tempor id eiusmod officia qui reprehenderit exercitation aliquip dolore qui nulla consequat.',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Non ad elit incididunt dolor aliquip aute officia exercitation cillum.',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Occaecat consequat ipsum minim cupidatat eu velit nostrud consequat elit nulla.'
+    }
+]
+
+export default FeedbackData
